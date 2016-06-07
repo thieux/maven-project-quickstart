29cc0dec7b1cf145b2204ac47e0c649822f667a4
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-06-07 15:09:38 +0200

    Setups the project with Maven 3, JUnit 11 on Java 8.

---
+++ src/main/java/com/mathieupauly/projectquickstart/App.java
@@ -0,0 +1,8 @@
+package com.mathieupauly.projectquickstart;
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/mathieupauly/projectquickstart/AppTest.java
@@ -0,0 +1,14 @@
+package com.mathieupauly.projectquickstart;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void canary() {
+        assertTrue(true);
+    }
+}
